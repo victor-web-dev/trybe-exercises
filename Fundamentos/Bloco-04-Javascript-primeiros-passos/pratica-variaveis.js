4a040f6ec9ca1fbcb0402d7565de5ba1f8ba2875
Victor Araujo <victor_emanuel9@hotmail.com>
2021-07-28 16:09:38 -0300

    pratica

---
+++ Fundamentos/Bloco-04-Javascript-primeiros-passos/pratica-variaveis.js
@@ -0,0 +1,12 @@
+const myName = "Victor", birthCity = "Aracaju";
+
+let birthYear = "1995";
+
+console.log(myName + '\n', birthCity + '\n', birthYear);
+
+birthYear = "2030";
+console.log(`Ano modificado: ${birthYear}`);
+
+birthCity = "nananana";
+
+console.log(birthCity); // nao roda pq constante nao muda de valor
