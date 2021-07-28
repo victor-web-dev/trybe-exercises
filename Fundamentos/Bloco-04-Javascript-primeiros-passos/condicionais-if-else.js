c86abc9ea540b15b92eba8837d1d7501b5367421
Victor Araujo <victor_emanuel9@hotmail.com>
2021-07-28 19:16:00 -0300

    if-else

---
+++ Fundamentos/Bloco-04-Javascript-primeiros-passos/condicionais-if-else.js
@@ -0,0 +1,9 @@
+let notaPessoa = 90;
+
+if(notaPessoa > 80){
+  console.log("voce foi aprovado");
+} else if(notaPessoa < 80 && notaPessoa >= 60){
+  console.log("Voce esta na lista de espera");
+}else{
+  console.log("voce foi reprovado");
+}
