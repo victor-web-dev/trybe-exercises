9671ee025fe6e99f39b2304067cf5863957835c3
Victor Araujo <victor_emanuel9@hotmail.com>
2021-07-29 13:05:30 -0300

    adicionado mais questoes

--- Fundamentos/Bloco-04-Javascript-primeiros-passos/exercicios.js
+++ Fundamentos/Bloco-04-Javascript-primeiros-passos/exercicios.js
@@ -4,9 +4,9 @@ let a,b,c;
 
 //while(!isEqual){
 
-a = (Math.random()*10).toFixed(0);
-b = (Math.random()*10).toFixed(0);
-c = (Math.random()*10).toFixed(0);
+a = parseInt((Math.random()*10));
+b = parseInt((Math.random()*10));
+c = parseInt((Math.random()*10));
 //compara maior, questao 2
 if(a > b){
   console.log(`${a} eh maior que ${b}` );
@@ -39,3 +39,69 @@ else {
 
 //ta com bug
 
+// questao 04
+//Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. 
+
+let num = (-1);
+
+if(num > 0){
+  console.log("valor positivo");
+}
+else if(num < 0){
+  console.log("valor negativo");
+}
+else{
+  console.log("valor igual a 0");
+}   
+
+ 
+//questao 05
+//Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro. 
+
+
+let lado1 = 60,
+    lado2 = 60,
+    lado3 = 60,
+    soma = lado1 + lado2 + lado3,
+    isValid = null;
+
+isValid = (soma === 180)?true:false;
+
+console.log(isValid);
+
+
+// questao 06
+//
+let chessPiece = 'pawn',
+    piece = chessPiece.toLowerCase();
+// options: pawn, rook, bishop, knight, king, queen
+
+switch(piece){
+  case 'pawn':
+    console.log('move-se sempre na vertical, 1 casa');
+    break;
+
+  case 'rook':
+    console.log('move-se na vertical e horizontal, quantas casas for possivel.');
+    break;
+
+  case 'bishop':
+    console.log('move-se na diagonal, quantas casa for possivel');
+    break;
+
+  case 'knight':
+    console.log('move-se na vertical e horizontal, podendo pular por cima de outra peça, 3 casas em forma de L. Se o movimento for vertical - anda-se 2 casa na vertical e a 3 precisa ser na horizontal. Se o movimento for horizontal - anda-se 2 casa na horizontal e a 3 precisa ser na vertical.');
+    break;
+
+  case 'king':
+    console.log('move-se em qualquer direcao 1 casa.');
+    break;
+
+  case 'queen':
+    console.log('move-se em qualquer direção seja vertical, horizontal ou diagonal.');
+    break;
+  
+  default:
+    console.log('valor invalido');
+    break;
+}
