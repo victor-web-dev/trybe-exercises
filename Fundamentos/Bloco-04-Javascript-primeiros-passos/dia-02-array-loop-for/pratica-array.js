910881f22d4a7d68d53e9b48c8c23b650a740101
Victor Araujo <victor_emanuel9@hotmail.com>
2021-07-29 15:38:38 -0300

    pratica array

---
+++ Fundamentos/Bloco-04-Javascript-primeiros-passos/dia-02-array-loop-for/pratica-array.js
@@ -0,0 +1,40 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+// // 3
+
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+// // Tomar café
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+// // Brincar com o cachorro
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+
+// tasksList.pop();  // remove a última tarefa
+// console.log(tasksList);
+
+// let indexOfTask = tasksList.indexOf('Reunião');
+// console.log(indexOfTask);
+
+//Exercício 1 
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+//Exercício 2 
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+//Exercício 3 
+
+menu.push('Contato');
+console.log(menu);
