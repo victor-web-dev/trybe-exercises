3e7db0b1a641c7475a87702015811115a4558be7
Victor Araujo <victor_emanuel9@hotmail.com>
2021-08-02 22:34:28 -0300

    fim exercicio part2

--- Fundamentos/Bloco-04-Javascript-primeiros-passos/dia-04-objetos-funcoes/exercicio-par2-function.js
+++ Fundamentos/Bloco-04-Javascript-primeiros-passos/dia-04-objetos-funcoes/exercicio-par2-function.js
@@ -128,3 +128,9 @@
 
 // 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
 
+function verificaFimPalavra(word, end ){
+  return word.endsWith(end);
+}
+
+console.log(verificaFimPalavra('trybe', 'be'));
+console.log(verificaFimPalavra('joaofernando', 'fernan'));
