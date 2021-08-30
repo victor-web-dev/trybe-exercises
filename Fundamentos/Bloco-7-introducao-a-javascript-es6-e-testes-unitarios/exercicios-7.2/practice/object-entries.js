58c0de88c490e667ee498dd1889369a1790b9600
Victor Araujo <victor_emanuel9@hotmail.com>
2021-08-30 15:28:24 -0300

    object.entries

---
+++ Fundamentos/Bloco-7-introducao-a-javascript-es6-e-testes-unitarios/exercicios-7.2/practice/object-entries.js
@@ -0,0 +1,16 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+//  console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('País:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
