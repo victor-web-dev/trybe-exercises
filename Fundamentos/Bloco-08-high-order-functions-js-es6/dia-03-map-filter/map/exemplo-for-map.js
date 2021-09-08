8d87250be60981afb78cb5843e6b9796a28fffcc
Victor Araujo <victor_emanuel9@hotmail.com>
2021-09-08 15:47:51 -0300

    exemplos de uso MAP e FILTER

---
+++ Fundamentos/Bloco-08-high-order-functions-js-es6/dia-03-map-filter/map/exemplo-for-map.js
@@ -0,0 +1,26 @@
+//  Como seria utilizando for
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = [];
+
+for (let index = 0; index < persons.length; index += 1) {
+  fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+}
+
+console.log(fullNames);
+
+
+// Como seria usando o map
+const persons = [
+  { firstName: 'Maria', lastName: 'Ferreira' },
+  { firstName: 'João', lastName: 'Silva' },
+  { firstName: 'Antonio', lastName: 'Cabral' },
+];
+
+const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+
+console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
---
+++ Fundamentos/Bloco-08-high-order-functions-js-es6/dia-03-map-filter/map/exemplo3-map-filter.js
@@ -0,0 +1,6 @@
+const allNameStudents = estudantes.filter((estudante) => (
+  estudante.turno === 'Manhã')).map((estudante) => `${estudante.nome} ${estudante.sobrenome}`);
+
+console.log(allNameStudents);
+
+/* Primeiro usou-se o filter para filtrar todos os estudantes que estudam no período da manhã. Como o retorno do filter é um array de elementos, você pode usar o map logo em seguida para retornar os nomes completos de estudantes presentes nesse novo array. O map nesse caso é usado apenas para retornar as informações pedidas, o nome completo, enquanto o filter é usado para filtrar o array. */
